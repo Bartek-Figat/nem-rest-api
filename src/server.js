eecd0219e0eef8d0a68de9dfbb47029d6ca1a992
Bartek-Figat <figat29@gmail.com>
2021-07-31 10:08:19 -0700

    V0.0.1

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const { userRouter } = require('./routes/index');
+const Port = process.env.Port || 8080;
+
+const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.use(userRouter);
+
+server.listen(Port, () => {
+  console.log(`Listening on port ${Port}`);
+});
