da20147b1caa6a4b3276f70650d03e7c120eb7ce
Bartek-Figat <figat29@gmail.com>
2021-08-04 22:55:43 -0700

    V0.0.6

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
+const compression = require('compression');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const pro = require('process');
 const { userRouter } = require('./routes/index');
 
 const Port = process.env.Port || 8080;
@@ -13,9 +17,26 @@ server.use(express.json());
 server.use(cors());
 server.use(helmet());
 server.use(morgan('dev'));
-
+server.use(compression());
 server.use(userRouter);
 
-server.listen(Port, () => {
-  console.log(`Listening on port ${Port}`);
-});
+if (cluster.isPrimary) {
+  console.log(`Primary ${pro.pid} is running`);
+
+  // Fork workers.
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  server.listen(Port, () => {
+    console.log(`Listening on port ${Port}`);
+  });
+
+  console.log(`Worker ${process.pid} started`);
+}
